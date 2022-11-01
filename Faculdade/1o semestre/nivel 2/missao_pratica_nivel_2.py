48beb8a71bbdbd9b33a82f965d6ffa56fc23a297
Amandio Capela <amandiocapela@gmail.com>
2022-11-01 17:57:00 -0300

    ajuste lista

--- Faculdade/1o semestre/nivel 2/missao_pratica_nivel_2.py
+++ Faculdade/1o semestre/nivel 2/missao_pratica_nivel_2.py
@@ -1,8 +1,7 @@
-n = eval(input("Digite um número\n"))
+n = eval(input("Digite um número:\n"))
 
 nresto = str(n % 2)
 nint = n // 2
-numero_base_binaria = list
 
 while nint != 0:
     nresto = nresto + str(nint % 2)
