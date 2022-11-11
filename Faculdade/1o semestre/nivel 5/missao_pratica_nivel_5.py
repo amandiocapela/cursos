b2a48648b892157e1c9d7a54f5ba7561ed689eb2
Amandio Capela <amandiocapela@gmail.com>
2022-11-11 18:33:19 -0300

    Leitura Nomes+Arquivos listas separadas

--- Faculdade/1o semestre/nivel 5/missao_pratica_nivel_5.py
+++ Faculdade/1o semestre/nivel 5/missao_pratica_nivel_5.py
@@ -7,12 +7,21 @@ n = int(input('Quantos nomes gostaria de gerar? '))
 
 with open('dados.txt', 'w') as arquivo:
     for c in range(0, n):
-        arquivo.write(fake.name() + ', ' + str(randint(1, n+1)) + '; ')
+        arquivo.write(f'{fake.name()}, {str(randint(1, n + 1))},')
 
 nomes = []
 pont = []
 
 with open('dados.txt', 'r') as arquivo:
-    a = arquivo.read().split(';')
+    a = arquivo.read().split(',')
+a.pop()
 
-print(a)
+for c in range(0, (n*2), 2):
+    nomes.append(a[c])
+    pont.append(a[c+1])
+
+
+print(nomes)
+print(pont)
+
+
