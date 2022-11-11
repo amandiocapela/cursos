79bb1ac4f25a2b0f51771cbcc2c4408fb45ff784
Amandio Capela <amandiocapela@gmail.com>
2022-11-11 17:55:49 -0300

    Nomes+Números criados em arquivo

--- Faculdade/1o semestre/nivel 5/missao_pratica_nivel_5.py
+++ Faculdade/1o semestre/nivel 5/missao_pratica_nivel_5.py
@@ -1,1 +1,18 @@
-print('Hello World')
+from matplotlib import pyplot as plt
+from faker import Faker
+from random import randint
+
+fake = Faker()
+n = int(input('Quantos nomes gostaria de gerar? '))
+
+with open('dados.txt', 'w') as arquivo:
+    for c in range(0, n):
+        arquivo.write(fake.name() + ', ' + str(randint(1, n+1)) + '; ')
+
+nomes = []
+pont = []
+
+with open('dados.txt', 'r') as arquivo:
+    a = arquivo.read().split(';')
+
+print(a)
