71a32408da8b2c7c9e4a447c2dfd744296a67cb2
Amandio Capela <amandiocapela@gmail.com>
2022-11-12 11:07:36 -0300

    Pandas para contar unicos e ajuste no bin

--- Faculdade/1o semestre/nivel 5/missao_pratica_nivel_5.py
+++ Faculdade/1o semestre/nivel 5/missao_pratica_nivel_5.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 from faker import Faker
 from random import randint
 from time import sleep
-from collections import Counter
+import pandas
 from num2words import num2words
 from wordcloud import WordCloud
 
@@ -17,7 +17,7 @@ n = int(input('Quantos nomes gostaria de gerar? '))
 print('Gerando nomes e números aleatórios...')
 
 # Gerar nomes e números aleatórios dentro de um arquivo txt
-fake = Faker()
+fake = Faker('pt_BR')
 with open('dados.txt', 'w') as arquivo:
     for c in range(0, n):
         arquivo.write(f'{fake.name()},{str(randint(1, 10))},')
@@ -39,7 +39,8 @@ for c in pont:
 
 # Gerar uma string única para o wordcloud gerar a núvem
 palavras = " ".join(p for p in num)
-wordcloud = WordCloud(background_color="black").generate(palavras)
+wordcloud = WordCloud(background_color="grey", width=800, height=400).generate(palavras)
+wordcloud.to_file("Nuvem de palavras.jpg")
 
 # Informar itens gerados
 sleep(1)
@@ -61,10 +62,11 @@ pont.sort(key=len)
 
 # Gerar gráficos
 plt.subplot(2, 1, 1)
-plt.hist(pont, label='Histograma das Pontuações')
+bbin = len(pandas.unique(pont))
+plt.hist(pont, bbin, density=True, facecolor='blue', label='Histograma das Pontuações')
 plt.xlabel('Pontuações')
 plt.ylabel('Probabilidade')
-plt.grid(f'{Counter(pont)}')
+plt.grid(True)
 plt.subplot(2, 1, 2)
 plt.imshow(wordcloud)
 plt.axis("off")
