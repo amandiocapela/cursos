6d25cfb5344d72274b8f49904abc698bc8dff57d
Amandio Capela <amandiocapela@gmail.com>
2023-01-05 07:01:44 -0300

    108 ao 109 + ajustes 107

--- Curso em Vídeo/Python 3/107/principal.py
+++ Curso em Vídeo/Python 3/107/principal.py
@@ -1,6 +1,6 @@
 import moedas
 
 p = float(input('Digite o preço: R$'))
-print(f'A metade de {p} é {moedas.metade(p)}')
-print(f'O dobro de {p} é {moedas.dobro(p)}')
-print(f'Aumentando 10% {p}, temos R${moedas.aumentar(p, 10)}')
+print(f'A metade de {p} é R${moedas.metade(p)}')
+print(f'O dobro de {p} é R${moedas.dobro(p)}')
+print(f'Aumentando 10% em {p}, temos R${moedas.aumentar(p, 10)}')
