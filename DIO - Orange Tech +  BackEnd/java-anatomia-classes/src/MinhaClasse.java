37064c1ba6973bf7e71b3c9dd2b9b340415e9b7c
Amandio Capela <amandiocapela@gmail.com>
2023-01-10 10:02:19 -0300

    Anatomia das Classes - 3

--- DIO - Orange Tech +  BackEnd/java-anatomia-classes/src/MinhaClasse.java
+++ DIO - Orange Tech +  BackEnd/java-anatomia-classes/src/MinhaClasse.java
@@ -1,10 +1,15 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        final String BR = "Brasil";
-        System.out.print(BR);
-        int ano = 2021;
-        System.out.print(ano);
-        ano = 2022;
-        System.out.print(ano);
+
+        String primeiroNome = "Amandio"; 
+        String segundoNome = "Capela";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
