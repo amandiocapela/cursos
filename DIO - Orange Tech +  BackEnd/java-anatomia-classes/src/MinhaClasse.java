c52135ea3a3258662bd0986f7238c4a981516b16
Amandio Capela <amandiocapela@gmail.com>
2023-01-10 08:16:50 -0300

    Anatomia das Classes - 2

--- DIO - Orange Tech +  BackEnd/java-anatomia-classes/src/MinhaClasse.java
+++ DIO - Orange Tech +  BackEnd/java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,10 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.print("Olá turma, sejam bem-vindos");
+        final String BR = "Brasil";
+        System.out.print(BR);
+        int ano = 2021;
+        System.out.print(ano);
+        ano = 2022;
+        System.out.print(ano);
     }
 }
