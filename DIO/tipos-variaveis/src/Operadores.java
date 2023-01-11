55cae299131279190ad74140d228052d4e00e00c
Amandio Capela <amandiocapela@gmail.com>
2023-01-11 07:22:37 -0300

    Operadores - 6

--- DIO/tipos-variaveis/src/Operadores.java
+++ DIO/tipos-variaveis/src/Operadores.java
@@ -1,13 +1,25 @@
 public class Operadores {
     public static void main(String[] args) {
         
-    int a, b;
-    a = 5;
-    b = 6;
+    int numero1 = 1;
+    int numero2 = 2;
 
-    String resultado = a==b ?"Verdadeiro" : "Falso";
+    boolean simNao = numero1 == numero2;
 
-    System.out.println(resultado);
+    if(numero1 < numero2){
+        System.out.println("A nossa condição é verdadeira");
+    }
+
+    System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+    simNao = numero1 != numero2;
+
+    System.out.println("numeroUm é diferente de numeroDois? " + simNao);
+
+    simNao = numero1 > numero2;
+
+    System.out.println("numeroUm é maior que numeroDois? " + simNao);
+
         
     }
     
